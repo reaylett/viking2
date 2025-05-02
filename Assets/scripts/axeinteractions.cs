a3e046be1a2290153c04504169940319b45e8228
reaylett <146747000+reaylett@users.noreply.github.com>
2025-05-02 17:54:31 +0100

    axe

---
+++ Assets/scripts/axeinteractions.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class axeinteractions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Hover()
+    {
+        Debug.Log("Hover");
+    }
+
+     public void Selected()
+    {
+        Debug.Log("Selected");
+    }
+
+     public void Activated()
+    {
+        Debug.Log("Activated");
+    }
+}
